69e05c438f2caa15459408cf2bfceaa98f17b6e7
lkh <lkh741@hotmail.com>
2018-06-28 11:06:14 +0800

    proc piano

--- x.asm
+++ x.asm
@@ -19,9 +19,14 @@ CODE SEGMENT
       ASSUME CS:CODE,DS:DATA
 
 START:
+
       mov ax,DATA
       mov ds,ax
-
+piano proc
+      push ax
+      push bx
+      push cx
+      push dx
      ; 改中断例程入口地址
       mov ax,0
       mov es,ax
@@ -29,13 +34,13 @@ START:
       pop ds:[0]
       push es:[9*4+2]
       pop ds:[2]
-      mov word ptr es:[9*4],offset NEWINT9
+      mov word ptr es:[9*4],offset piNEWINT9
       mov es:[9*4+2],cs
-LOOPMAIN:
-      jmp LOOPMAIN   ;死循环，等待中断到来
+piLOOPMAIN:
+      jmp piLOOPMAIN   ;死循环，等待中断到来
 
 ; 定义中断例程
-NEWINT9:
+piNEWINT9:
       push ax
       push bx
       push dx
@@ -47,36 +52,28 @@ NEWINT9:
       and bh,11111100b
       push bx
       popf
-      call dword ptr ds:[0]   ;调用原int9中断
+      call dword ptr ds:[0]   ;调用原int9中断s
 
       mov bl, al ;保存al
 
-MAIN:      ;按下或松开事件处理主要内容
-      
-
-      ;显示按下的扫描码
-      ; push ax
-      ; mov dl,al
-      ; mov ah,2
-      ; int 21H
-      ; pop ax
-JUDGE0:
+piMAIN:      ;按下或松开事件处理主要内容
+piJUDGE0:
       cmp al,0bH  ;判断是否按下0  0的扫描码是0bH  按下0结束程序
-      jnz JUDGE1
-      jmp EXIT
-JUDGE1:     ;判断是否为按下SHIFT
+      jnz piJUDGE1
+      jmp piEXIT
+piJUDGE1:     ;判断是否为按下SHIFT
       cmp al,2aH  ;2aH是SHIFT的扫描码
-      jnz JUDGE2
+      jnz piJUDGE2
       mov dl,1
       mov SHIFT,dl    ;用dx记录SHIFT状态 1为按下
-      jmp EXITNEWINT9
-JUDGE2:     ;判断是否为松开SHIFT
+      jmp piEXITNEWINT9
+piJUDGE2:     ;判断是否为松开SHIFT
       cmp al,0aaH  ;aaH是SHIFT的断码
-      jnz VOICE
+      jnz piVOICE
       mov dl,0
       mov SHIFT,dl    ;用dx记录SHIFT状态 0为松开
-      jmp EXITNEWINT9
-VOICE:     
+      jmp piEXITNEWINT9
+piVOICE:     
       ;cmp al,02H ; 02h是1的扫描码//
       ;在TABSCAN中查找有没有与按下的键的扫描码对应的值
       ;push dx
@@ -90,7 +87,7 @@ VOICE:
       mov al,bl   ;串扫描字节(AL)-(ES:[DI])
       repne scasb
       
-      jnz release
+      jnz pirelease
       
       ;计算输入字符在tabscan中对应的偏移量
       dec di
@@ -101,33 +98,29 @@ VOICE:
       ;检测按下字符时SHIFT的状态
       mov dl,SHIFT
       cmp dl,1
-      jnz ORIGIN  ;不等于1  跳转到ORIGIN（不做升调处理）
+      jnz piORIGIN  ;不等于1  跳转到ORIGIN（不做升调处理）
       ;否则升调（除了其中6种情况）
       mov dl,bl   ;扫描码放入dl中
       cmp dl,04H  ;判断是不是SHIFT+3
-      jz release
+      jz pirelease
       cmp dl,12H  ;判断是不是SHIFT+e
-      jz release
+      jz pirelease
       cmp dl,20H  ;判断是不是SHIFT+d
-      jz release
+      jz pirelease
       cmp dl,08H  ;判断是不是SHIFT+7
-      jz release
+      jz pirelease
       cmp dl,16H  ;判断是不是SHIFT+u
-      jz release
+      jz pirelease
       cmp dl,24H  ;判断是不是SHIFT+j
-      jz release
+      jz pirelease
 
       ;升调处理
       inc ax
 
-ORIGIN:
+piORIGIN:
       mov di,ax
-      ;add di,di
-      ;mov dx,di   ;用dx暂存频率表TABVAL的偏移量
       pop es
       pop ax
-      ;pop dx
-      ;jne release
       
       push ax
       push bx
@@ -154,18 +147,14 @@ ORIGIN:
       mov al,ah
       out 42H,al;写低字节，然后就响了
 
-      
       pop bx
       pop ax
       
-      jmp EXITNEWINT9
+      jmp piEXITNEWINT9
 
-release:    ;释放事件
+pirelease:    ;释放事件
       pop es
       pop ax 
-
-      ; cmp al,82h ; 82h是1的断码
-      ; jne EXIT
       
       push ax
       
@@ -173,14 +162,12 @@ release:    ;释放事件
       in al,61H;             关闭与门  
       and al,0FCH;  
       out 61H,al; 
-
-
       pop ax
-      jmp EXITNEWINT9
+      jmp piEXITNEWINT9
 
- EXIT:
+ piEXIT:
       ; cmp bl, 0bh ;4Fh是EXIT键的扫描码;0bh是0的扫描码
-      ; jne EXITNEWINT9
+      ; jne piEXITNEWINT9
       ;处理EXIT，使程序结束，注意在此要恢复中断向量
       mov ax,0
       mov es,ax
@@ -190,15 +177,19 @@ release:    ;释放事件
       push ds:[2]
       pop es:[9*4+2]
 
-      mov ah,4ch
-      int 21h
+      pop dx
+      pop cx
+      pop bx
+      pop ax
+      ret
 
-EXITNEWINT9:      ;结束该次响应9
+piEXITNEWINT9:      ;结束该次响应9
       pop es
       pop dx
       pop bx
       pop ax
       iret
+piano endp
 
 CODE ENDS
     END START
